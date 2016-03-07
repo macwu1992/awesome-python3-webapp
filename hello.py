f6a20bae35218d960b70bf508c4d56f6bafde102
mawu1992 <mawu1992.hotmail.com>
2016-03-07 08:33:56 +0800

    sql 测试语句

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 import orm, asyncio
-from models import User
+from models import User, Blog
 
 def test(loop):
    yield from orm.create_pool(loop=loop, user='root', password='', db='awesome')
-   users = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+   users = User(name='Test1', email='test@example.com1', passwd='12345678901', image='about:blank1')
+   blog = Blog(user_id = '123')
 
    yield from users.save()
+   yield from blog.save()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(test(loop))
+test(loop)
 loop.close()
